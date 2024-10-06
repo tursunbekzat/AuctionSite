348f6e34d08824f30eb8d0c6afe811488858151d
tursunbekzat <b_tursun@kbtu.com>
2024-10-06 12:53:49 +0500

    creating models and adding to admin panel

--- AS/core/admin.py
+++ AS/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import User, Category, Product, Bid, Order, OrderItem
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'current_price', 'author', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'amount', 'created_at')  # Поля для отображения
+    list_filter = ('product',)  # Фильтрация по продукту
+
+
+admin.site.register(User)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- AS/core/models.py
+++ AS/core/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Измените на уникальное имя
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Измените на уникальное имя
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    
+    # Начальная цена товара
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    # Текущая цена (последняя ставка)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    author = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        if self.current_price is None:  # Устанавливаем начальную цену как текущую при создании
+            self.current_price = self.starting_price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Bid(models.Model):
+    product = models.ForeignKey(Product, related_name='bids', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='bids', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Проверка, что пользователь не может делать ставку на свой продукт
+        if self.product.author == self.user:
+            raise ValidationError("Вы не можете делать ставку на свой продукт.")
+        else:
+            # Проверяем, что ставка больше текущей цены продукта
+            if self.amount <= self.product.current_price:
+                raise ValueError("Ставка должна быть выше текущей цены.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Bid by {self.user.username} for {self.product.name} - ${self.amount}"
+    
+    
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f'Order {self.id} by {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена товара на момент заказа
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
