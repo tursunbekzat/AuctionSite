18ba53579eacfab4ed43e6fb04e41fe054286e78
chriskyle <alikhanov.2004@gmail.com>
2024-10-06 17:22:20 +0500

    Create Auction and Current Auctions Templates and Styling

--- AS/core/models.py
+++ AS/core/models.py
@@ -25,11 +25,9 @@ class Product(models.Model):
     category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-    
-    # Начальная цена товара
+
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
     
-    # Текущая цена (последняя ставка)
     current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     author = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
