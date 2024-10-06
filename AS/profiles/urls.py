1496138a7ab04ac45f95cf76b1bcfa469fbe95b5
tursunbekzat <b_tursun@kbtu.com>
2024-10-06 17:03:07 +0500

    UD bid

--- AS/profiles/urls.py
+++ AS/profiles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('my-profile/', views.my_profile, name='my_profile'),
-    path('my-auctions/', views.my_auctions, name='my_auctions'),
-    path('my-bids/', views.my_bids, name='my_bids'),
+    path('my-profile/', my_profile, name='my_profile'),
+    path('my-auctions/', my_auctions, name='my_auctions'),
+    path('my-bids/', my_bids, name='my_bids'),
+    path('edit-bid/<int:bid_id>/', edit_bid, name='edit_bid'),  
+    path('delete-bid/<int:bid_id>/', delete_bid, name='delete_bid'),
 ]
--- AS/profiles/views.py
+++ AS/profiles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from core.models import Product, Bid
-
+from .forms import BidForm
 
 @login_required
 def my_profile(request):
@@ -19,3 +20,38 @@ def my_auctions(request):
 def my_bids(request):
     bids = Bid.objects.filter(user=request.user)
     return render(request, 'profiles/my_bids.html', {'bids': bids})
+
+
+# Изменение ставки
+@login_required
+def edit_bid(request, bid_id):
+    bid = get_object_or_404(Bid, id=bid_id)
+
+    # Проверяем, что текущий пользователь является автором ставки
+    if bid.user != request.user:
+        raise PermissionDenied("Вы не можете редактировать чужие ставки.")
+
+    if request.method == 'POST':
+        form = BidForm(request.POST, instance=bid)
+        if form.is_valid():
+            form.save()
+            return redirect('my_bids')  # Перенаправляем на страницу с вашими ставками
+    else:
+        form = BidForm(instance=bid)
+
+    return render(request, 'profiles/edit_bid.html', {'form': form, 'bid': bid})
+
+# Удаление ставки
+@login_required
+def delete_bid(request, bid_id):
+    bid = get_object_or_404(Bid, id=bid_id)
+
+    # Проверяем, что текущий пользователь является автором ставки
+    if bid.user != request.user:
+        raise PermissionDenied("Вы не можете удалять чужие ставки.")
+
+    if request.method == 'POST':
+        bid.delete()
+        return redirect('my_bids')  # Перенаправляем на страницу с вашими ставками
+
+    return render(request, 'profiles/delete_bid.html', {'bid': bid})
