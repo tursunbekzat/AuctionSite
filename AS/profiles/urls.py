fcba94c76d37acc943bda697e86da3a141ff1d19
tursunbekzat <b_tursun@kbtu.com>
2024-10-06 18:10:28 +0500

    profiles updated

--- AS/profiles/urls.py
+++ AS/profiles/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('my-bids/', my_bids, name='my_bids'),
     path('edit-bid/<int:bid_id>/', edit_bid, name='edit_bid'),  
     path('delete-bid/<int:bid_id>/', delete_bid, name='delete_bid'),
+    path('auctions/<int:auction_id>/update/', update_auction_view, name='update_auction'),
+    path('auctions/<int:auction_id>/delete/', delete_auction_view, name='delete_auction'),
+    path('auctions/<int:auction_id>/extend/', extend_auction_view, name='extend_auction'),
 ]
