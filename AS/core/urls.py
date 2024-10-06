dff64f7a5dbd98f044c160315527444c6721f8d3
tursunbekzat <b_tursun@kbtu.com>
2024-10-06 13:10:40 +0500

    merging templates

--- AS/core/urls.py
+++ AS/core/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    url('home/', views.home, name='home'),
+    path('home/', views.home, name='home'),
 ]
